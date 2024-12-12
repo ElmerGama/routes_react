bb1b0d4808d0877f86c97f3ef3c3adb90b85ed16
ElmerGama <43096999+ElmerGama@users.noreply.github.com>
2024-12-12 13:24:51 +0000

    ajuste de react doom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -7,53 +7,26 @@ import Contact from './components/Contact';
 const App = () => {
     return (
         <Router>
-            <nav style={styles.nav}>
-                <ul style={styles.ul}>
-                    <li style={styles.li}>
-                        <Link to="/" style={styles.link}>Inicio</Link>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
                     </li>
-                    <li style={styles.li}>
-                        <Link to="/about" style={styles.link}>Acerca de</Link>
+                    <li>
+                        <Link to="/about">About</Link>
                     </li>
-                    <li style={styles.li}>
-                        <Link to="/contact" style={styles.link}>Contacto</Link>
+                    <li>
+                        <Link to="/contact">Contact</Link>
                     </li>
                 </ul>
             </nav>
-            <div style={styles.container}>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/contact" element={<Contact />} />
-                </Routes>
-            </div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
         </Router>
     );
-};
-
-const styles = {
-    nav: {
-        backgroundColor: '#282c34',
-        padding: '1rem',
-    },
-    ul: {
-        listStyle: 'none',
-        display: 'flex',
-        justifyContent: 'center',
-        margin: 0,
-        padding: 0,
-    },
-    li: {
-        margin: '0 1rem',
-    },
-    link: {
-        color: '#61dafb',
-        textDecoration: 'none',
-        fontSize: '1.2rem',
-    },
-    container: {
-        padding: '2rem',
-    },
 };
 
 export default App;
